a82b5a05e93b884274260d3a082d4a665e34a4ed
Thomas Upton <thomasupton@gmail.com>
2019-03-05 11:20:44 -0600

    Project management

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "root": true,
+    "env": {
+        "browser": "true"
+    },
+    "extends": [
+        "eslint:recommended",
+        "airbnb"
+    ],
+    "rules": {
+        "react/react-in-jsx-scope": "off"
+    }
+};
